781443a145514367187f0362d24219c502e95bef
robertwatt <iamrobertwatt@gmail.com>
2021-02-12 00:39:45 +0000

    humanized time ago method

--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -6,16 +6,18 @@ class FinstagramPost < ActiveRecord::Base
 
    validates :photo_url, :user, presence: true
 
-    def humanized_time_ago
-        time_ago_in_seconds = Time.now - self.created_at
-        time_ago_in_minutes = time_ago_in_seconds / 60
-
-        if time_ago_in_minutes >= 60
-            " #{(time_ago_in_minutes / 60).to_i} hours ago"
-        else
-            "#{time_ago_in_minutes.to_i} minutes ago"
-        end
-    end
+  def humanized_time_ago
+  time_ago_in_seconds = (Time.now - self.created_at).to_i
+  case time_ago_in_seconds
+    when 0..59 then "#{time_ago_in_seconds} seconds ago"       
+    when 60..119 then "#{time_ago_in_seconds / 60} minute ago"
+    when 120..3599 then "#{time_ago_in_seconds / 60} minutes ago"
+    when 3600..7199 then "#{time_ago_in_seconds / 3600} hour ago"
+    when 7200..86399 then "#{time_ago_in_seconds / 3600} hours ago"
+    when 86400..172799 then "#{time_ago_in_seconds / 86400} day ago"
+    else "#{time_ago_in_seconds / 86400} days ago"
+  end
+end
 
     def like_count
         self.likes.size
